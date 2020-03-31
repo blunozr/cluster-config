dc5ed61c45658319b5d389064b0ffbf180efa5fd
developer <gnunn@redhat.com>
2020-03-31 14:30:21 -0700

    Fix directory

--- replace-sealed-secrets-secret.sh
+++ replace-sealed-secrets-secret.sh
@@ -4,7 +4,7 @@ echo "Deleting existing secret."
 oc delete secret -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key
 
 echo "Creating secret from local drive."
-oc create -f ~/secrets/bitnami/sealed-secrets-secret.yaml -n sealed-secrets
+oc create -f ~/.bitnami/sealed-secrets-secret.yaml -n sealed-secrets
 
 echo "Restarting Sealed Secrets controller."
 oc delete pod -l name=sealed-secrets-controller -n sealed-secrets
