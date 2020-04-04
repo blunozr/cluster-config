9941fa1756da0d35d36b3cc007b891031a455f50
Andrew Pitt <apitt@redhat.com>
2020-04-04 09:51:49 -0400

    Tweaked get-sealed-secret-key script.

--- get-sealed-secret-key.sh
+++ get-sealed-secret-key.sh
@@ -3,10 +3,10 @@
 echo "Getting public key from Sealed Secrets secret and copying it to ~/bitnami"
 
 echo "Create dir for Sealed Secrets public key. (~/bitnami)."
-mkdir -p ~/bitnami
+mkdir -p ~/.bitnami
 
 echo "Backup secret itself"
-oc get secret -o yaml -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key > ~/.bitnami/sealed-secrets-secret.yaml
+oc get secret -o yaml -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key=active > ~/.bitnami/sealed-secrets-secret.yaml
 
 echo "Get the public key from the Sealed Secrets secret."
-oc get secret -o yaml -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key | grep tls.crt | cut -d' ' -f6 | base64 --decode > ~/.bitnami/publickey.pem
+oc get secret -o yaml -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key=active | grep tls.crt | cut -d' ' -f6 | base64 --decode > ~/.bitnami/publickey.pem
